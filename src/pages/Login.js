a43ecd4e48553cf0a1dc2e3ff7084a557c37adc6
Demand Btc <102271199+Demandtech@users.noreply.github.com>
2023-03-29 11:09:41 +0100

    feat:form validation functionality implementation

---
+++ src/pages/Login.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import GuessLayout from '@/components/GuessLayout'
+
+const Login = () => {
+  return (
+    <GuessLayout>Login</GuessLayout>
+  )
+}
+
+export default Login
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,43 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
-//import '@/styles/globals.css'
-import Link from 'next/link'
 import GuessLayout from '@/components/GuessLayout'
 import Header from '@/components/Header'
+import { useState } from 'react'
+
+let regex =
+  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
 export default function Home() {
+  const [value, setValue] = useState('')
+  const [isFocus, setIsFocus] = useState({label: false, outline: false })
+  const [inputError, setInputError] = useState({ show: false, msg: '', match:false })
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
+  const handleFocus = () => {
+    setIsFocus({ label: true, outline: true })
+  }
+
+  const handleBlur = () => {
+    if (!value) {
+      setIsFocus({ label: false, outline: false })
+      setInputError({ show: true, msg: 'Email is required', match: false })
+    } else if(value) {
+      setIsFocus({ label: true, outline: false })
+    }
+
+    if(value.toLowerCase().match(regex)){
+      console.log('error')
+      setInputError({ show: false, msg: '', match:true })
+    }else{
+      setInputError({ show: true, msg: 'Please enter a valid email', match: false })
+    }
+  }
+
+  const handleChange = (e) => {
+    setValue(e.target.value)
+  }
   return (
     <GuessLayout>
       <div className={styles.hero}>
@@ -15,19 +46,40 @@ export default function Home() {
           <h1>Unlimited movies, TV shows, and more.</h1>
           <p>Watch anywhere. Cancel anytime.</p>
 
-          <form action='' className={styles.form}>
+          <form action='' className={styles.form} onSubmit={handleSubmit}>
             <h3>
               Ready to watch? Enter your email to create or restart your
               membership.
             </h3>
             <div className={styles['form-control']}>
-              <div className={styles['input-control']}>
-                <div className={styles['input-label']}>
-                  <label htmlFor='email'>Email Address</label>
+            
+                <div
+                  className={`${styles['input-control']} ${
+                    isFocus.outline ? `${styles['input-control-focus']}` : ''
+                  } ${inputError.show ? `${styles['input-error']}` : ''} ${
+                    inputError.match ? `${styles['input-match']}` : ''
+                  }`}
+                  >
+                  <div
+                    className={`${styles['input-label']} ${
+                      isFocus.label ? `${styles['input-focus']}` : ``
+                    }`}
+                  >
+                    <label htmlFor='email'>Email Address</label>
+                  </div>
+                  <input
+                    value={value}
+                    id='email'
+                    type='text'
+                    name='email'
+                    onChange={handleChange}
+                    onFocus={handleFocus}
+                    onBlur={handleBlur}
+                    autoComplete={'off'}
+                  />
                 </div>
-                <input id='email' type='text' name='email' />
-                <div className='input-layer'></div>
-              </div>
+                
+            
               <div className={styles['btn-wrapper']}>
                 <button className='btn'>Get Started &gt;</button>
               </div>
